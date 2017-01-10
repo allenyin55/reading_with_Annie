1e9d891871d200f414dbbcb3e4a1ae22dc83d088
Allen Yin <yinhaonan55@gmail.com>
2017-01-09 21:29:06 -0800

    moment

--- src/utils/jwtHelper.js
+++ src/utils/jwtHelper.js
@@ -1,13 +1,14 @@
 import decode from 'jwt-decode';
 
 export function getTokenExpirationDate(token) {
-    console.log("start of the getToken");
+    console.log(token);
     const decoded = decode(token);
+    console.log(decoded);
+    console.log(decoded.exp)
     if(!decoded.exp) {
         console.log('return null')
         return null
     }
-
     const date = new Date(0); // The 0 here is the key, which sets the date to the epoch
     date.setUTCSeconds(decoded.exp);
     return date
@@ -17,9 +18,8 @@ export function isTokenExpired(token) {
     const date = getTokenExpirationDate(token);
     const offsetSeconds = 0;
     if (date === null) {
-        console.log("token expired")
+        console.log("token expired");
         return false
     }
-    console.log('end of isTokenExpired');
     return !(date.valueOf() > (new Date().valueOf() + (offsetSeconds * 1000)))
 }
