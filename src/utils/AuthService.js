d2064e8b94f2d029fd3d89aa54bc50a810c32109
Allen Yin <yinhaonan55@gmail.com>
2017-01-09 22:14:53 -0800

    moment

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -5,6 +5,8 @@ import { EventEmitter } from 'events';
 
 export default class AuthService extends EventEmitter{
     constructor(clientId, domain) {
+        console.log(clientId)
+        console.log(domain)
         super();
         // Configure Auth0
         this.lock = new Auth0Lock(clientId, domain, {
